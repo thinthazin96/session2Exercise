24f0cf300ff0acdb9902dd71fa8648207968b9fc
codingsake <thinthazin.chris@gmail.com>
2022-01-04 14:30:32 -0500

    Answer for 3 to 5

---
+++ session2.py
@@ -0,0 +1,16 @@
+s = "Hello World"
+
+print("First Slice: " + s[0:5])
+print("Second Slice: " + s[6:11])
+
+print("Reverse string: " + s[::-1])
+
+user = input("This will reverse whatever you typed in: ")
+print(user[::-1])
+
+name = input("What is your name? ")
+print(f"Hello {name}! Have a great day.")
+
+print(type(1))
+print(type("Hello World"))
+print(type(True))
