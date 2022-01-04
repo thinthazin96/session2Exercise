a456aa0ebc204bb4607a7a877f95655157dcb254
codingsake <thinthazin.chris@gmail.com>
2022-01-04 14:45:17 -0500

    Answer from 6 to 10

--- session2.py
+++ session2.py
@@ -1,16 +1,38 @@
-s = "Hello World"
+# s = "Hello World"
 
-print("First Slice: " + s[0:5])
-print("Second Slice: " + s[6:11])
+# print("First Slice: " + s[0:5])
+# print("Second Slice: " + s[6:11])
 
-print("Reverse string: " + s[::-1])
+# print("Reverse string: " + s[::-1])
 
-user = input("This will reverse whatever you typed in: ")
-print(user[::-1])
+# user = input("This will reverse whatever you typed in: ")
+# print(user[::-1])
 
-name = input("What is your name? ")
-print(f"Hello {name}! Have a great day.")
+# name = input("What is your name? ")
+# print(f"Hello {name}! Have a great day.")
 
-print(type(1))
-print(type("Hello World"))
-print(type(True))
+# print(type(1))
+# print(type("Hello World"))
+# print(type(True))
+
+#Problem 6
+a = 3
+b = 4
+# print(a + b)
+
+#Problem 7
+# j = 1
+# while j < 6:
+#     print(j ** 2)
+#     j = j + 1
+
+#Problem 8
+# j = 1
+# while j < 6:
+#     print(j // 2)
+#     j = j + 1
+
+#Problem 9 & 10 
+lst1 = ["a","b","c"]
+lst2 = ["d","e","f"]
+print(lst1 + lst2)
